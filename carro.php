5ecde5dac7293b9383c914bb40df02e896807b55
Jorge <jag7@outlook.es>
2019-06-07 19:45:29 -0500

    Add files via upload

---
+++ carro.php
@@ -0,0 +1,21 @@
+<!--
+JAGProg
+Ejercicios para el aprendizaje
+
+-->
+
+<?php
+	
+	
+// obtiene los datos de entrada del formulario
+$tipo = $_POST['tipo'];
+$txtColor = $_POST['txtColor'];
+// utiliza los datos de entrada del formulario
+echo "Tu $tipo $txtColor está listo. ¡Disfrutalo!";
+echo "<br />";
+	echo "<br />";
+	echo "<a href='carro.html'>Volver</a>";
+
+
+
+?>
